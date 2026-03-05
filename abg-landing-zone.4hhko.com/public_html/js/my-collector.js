e63082895c77b93a7f8ab01cc5a4f7629f8238ee
EdwardLoeber <eloeber@ucsd.edu>
2026-03-04 16:52:32 -0800

    asdfads

--- abg-landing-zone.4hhko.com/public_html/js/my-collector.js
+++ abg-landing-zone.4hhko.com/public_html/js/my-collector.js
@@ -9,15 +9,8 @@
         payload.cookieEnabled  = navigator.cookieEnabled;
         payload.language       = navigator.language;
         payload.allowsJS       = true;
-        payload.allowsImages1  = detectImagesEnabled1();
-        payload.allowsImages2  = detectImagesEnabled2();
-
-        const flag = document.getElementById('detectImageFlag');
-        console.log('flag element:', flag);
-        console.log('offsetWidth:', flag ? flag.offsetWidth : 'no element');
-        console.log('readyState:', flag ? flag.readyState : 'no element');
-        console.log('naturalWidth:', flag ? flag.naturalWidth : 'no element');
-        console.log('complete:', flag ? flag.complete : 'no element');
+        payload.allowsImages  = detectImagesEnabled();
+        
         
         if (debugging === true) {
             console.log(payload);
@@ -30,22 +23,12 @@
         }
     }
 
-    function detectImagesEnabled1() {
+    function detectImagesEnabled() {
         const flag = document.getElementById('detectImageFlag');
         if (!flag) return null;
         return (flag.complete && flag.naturalWidth > 0);
     }
 
-    function detectImagesEnabled2() {
-        try {
-            const img = new Image();
-            img.src = 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7';
-            return img.complete && img.naturalWidth > 0;
-        } catch (e) {
-            return false;
-        }
-    }
-    
     if (debugging === true) {
         console.log(payload);
     } else {
