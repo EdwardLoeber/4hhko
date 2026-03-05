a739e0f0ba24ace968a15f38121eae3f4a5fb8b5
EdwardLoeber <eloeber@ucsd.edu>
2026-03-04 16:20:27 -0800

    detect images

--- abg-landing-zone.4hhko.com/public_html/js/my-collector.js
+++ abg-landing-zone.4hhko.com/public_html/js/my-collector.js
@@ -9,8 +9,7 @@
         payload.cookieEnabled  = navigator.cookieEnabled;
         payload.language       = navigator.language;
         payload.allowsJS       = true;
-        payload.allowsImages1  = detectImagesEnabled1();
-        payload.allowsImages2  = detectImagesEnabled2();
+        payload.allowsImages  = detectImagesEnabled();
 
         if (debugging === true) {
             console.log(payload);
@@ -23,14 +22,7 @@
         }
     }
 
-    function detectImagesEnabled1() {
-        const flag = document.getElementById('detectImageFlag');
-        if (!flag) return null;
-        return (flag.offsetWidth === 1 && flag.readyState === 'complete')
-            || (flag.offsetWidth === 1 && flag.readyState === undefined);
-    }
-
-    function detectImagesEnabled2() {
+    function detectImagesEnabled() {
         try {
             const img = new Image();
             img.src = 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7';
