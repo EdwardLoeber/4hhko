b2a75a946603b59ffe75de35def661997cddd041
EdwardLoeber <eloeber@ucsd.edu>
2026-03-15 23:47:09 -0700

    asdf

--- reporting.4hhko.com/public_html/dashboard.php
+++ reporting.4hhko.com/public_html/dashboard.php
@@ -2,8 +2,4 @@
 // Controller: authenticate, then delegate entirely to the view.
 define('IN_APP', true);
 require_once 'auth.php';
-if (CURRENT_USER_ROLE === 'viewer') {
-    header('Location: /saved.php');
-    exit;
-}
 require_once 'views/dashboard-view.php';
--- reporting.4hhko.com/public_html/insights.php
+++ reporting.4hhko.com/public_html/insights.php
@@ -1,5 +1,5 @@
 <?php
 define('IN_APP', true);
 require_once 'auth.php';
-if (CURRENT_USER_ROLE === 'viewer') { header('Location: /saved.php'); exit; }
+
 require_once 'views/insights-view.php';
